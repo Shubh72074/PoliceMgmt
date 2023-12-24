e88ee8e537565e82b296c3666fed07cc20d38782
Shubh72074 <Shubhamnishadr16@gmail.com>
2023-12-24 23:51:50 +0530

    new changes

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask,jsonify,render_template,request,redirect,session,url_for,sessions,Response
-import mysql as sql
+from flask_mysqldb import MySQL
+import json
 
 app = Flask(__name__)
 app.secret_key = 'C@NYoUh@cktHiS'
+
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "Demon@72074"
+app.config["MYSQL_DB"] = "policemngmt"
+
+mysql = MySQL(app)
 
 @app.route('/')
 def index():
@@ -22,14 +29,6 @@ def admin():
         return redirect(page+'/dashboard')
     return render_template('admin/index.html')
 
-@app.route('/roles')
-def user_roles():
-  roles_date = {
-    1:"data",
-    2:"nodata"
-  }
-  
-  return jsonify(roles_data)
 
 @app.route('/police',methods=['GET','POST'])
 def police():
@@ -44,9 +43,19 @@ def police():
         page = session['user']
         return redirect(page+'/dashboard')
     return render_template('police/index.html')
-
-@app.route('/user')
+    
+@app.route('/user',methods=['GET','POST'])
 def user():
+  if request.method == "POST":
+    session['user'] = 'user'
+    return redirect('user/dashboard')
+  if request.method == 'GET':
+    if session.get('user') :
+      if session['user'] == 'user':
+        return redirect('user/dashboard')
+      else:
+        page = session['user']
+        return redirect(page+'/dashboard')
   return render_template('user/index.html')
 
 @app.route('/admin/logout')
@@ -59,26 +68,77 @@ def policeLogout():
    session.pop('user',None)
    return redirect(url_for('index'))
 
+@app.route('/user/logout')
+def userLogout():
+   session.pop('user',None)
+   return redirect(url_for('index'))
+
 @app.route('/police/<page>')
 def police_dashboard(page):
-  return render_template('police/dashboard.html',officer_name="Shubham Nishad",url=page,ps_counts=len(p_stations),c_counts=10,fir_counts=3,po_counts=5)
+  return render_template('police/dashboard.html',url=page)
 
 @app.route('/admin/<page>')
 def admin_dashboard(page):
-  return render_template('admin/dashboard.html',url=page,ps_counts=len(p_stations),c_counts=10,fir_counts=3,po_counts=5)
+  return render_template('admin/dashboard.html',url=page)
 
+@app.route('/user/<page>')
+def user_dashboard(page):
+  return render_template('user/dashboard.html',url=page)
+
+def countPS():
+  return len(api_police_stations())
 
 @app.route('/api/police_stations',methods=['GET'])
 def api_police_stations():
+  cur = mysql.connection.cursor()
+  cur.execute("""SELECT * FROM policemngmt.police_stations""")
+  row_headers=[x[0] for x in cur.description]
+  rv = cur.fetchall()
+  p_stations=[]
+  for result in rv:
+    p_stations.append(dict(zip(row_headers,result)))
+  cur.close()
   return jsonify(p_stations)
 
 @app.route('/api/police',methods=['GET'])
 def api_police():
+  cur = mysql.connection.cursor()
+  cur.execute("""SELECT * FROM policemngmt.p_officers""")
+  row_headers=[x[0] for x in cur.description]
+  rv = cur.fetchall()
+  p_officers=[]
+  for result in rv:
+    p_officers.append(dict(zip(row_headers,result)))
+  cur.close()
   return jsonify(p_officers)
 
 @app.route('/api/categories')
 def api_categories():
+  cur = mysql.connection.cursor()
+  cur.execute("""SELECT * FROM policemngmt.crime_categories""")
+  row_headers=[x[0] for x in cur.description]
+  rv = cur.fetchall()
+  crime_categories=[]
+  for result in rv:
+    crime_categories.append(dict(zip(row_headers,result)))
+  cur.close()
   return jsonify(crime_categories)
+
+@app.route('/api/roles')
+def user_roles():
+  cur = mysql.connection.cursor()
+  cur.execute("""SELECT * FROM policemngmt.user_roles""")
+  row_headers=[x[0] for x in cur.description]
+  rv = cur.fetchall()
+  json_data=[]
+  for result in rv:
+    json_data.append(dict(zip(row_headers,result)))
+  cur.close()
+  return jsonify(json_data)
+
+
+
+
 
 if __name__ == '__main__':
   app.run(debug=True)
